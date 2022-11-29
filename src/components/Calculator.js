54f9581779e0e83c7322169b5591618d05189c28
V-Blaze <valentine6586@gmail.com>
2022-11-29 10:55:16 +0100

    set up calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Calculator.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+    );
+  }
+}
+
+export default Calculator;
