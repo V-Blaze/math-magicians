c60c2e66726f30e9f14eae18caeb2f94cd06c436
V-Blaze <valentine6586@gmail.com>
2022-12-07 13:52:52 +0100

    write unit test for operate Js

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,1 +1,15 @@
 import React from "react";
+import operate from "./operate";
+
+describe('Test operatorJs logic', () => {
+    test('15 divided by 3 to give 5', () => { 
+        let result = operate("15", "3", "÷")
+
+        expect(result).toEqual('5')
+     })
+    test('Multiply 20 by 5 to get 100', () => { 
+        let result = operate("20", "5", "x")
+
+        expect(result).toEqual('100')
+     })
+});
